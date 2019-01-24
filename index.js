2909fdec9b6f9829e8fc39949074ee94f889c3e3
Jason <jikurata@gmail.com>
2019-01-23 16:22:09 -0800

    resolvePath now correctly resolves root relative paths

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const HtmlCompiler = require('./lib/HtmlCompiler.js');
 
 module.exports = HtmlCompiler;
+
+const c = new HtmlCompiler({url: 'http://mysite.com'});
+const s = c.compile('__test__/example/example1.html');
+console.log(s);
